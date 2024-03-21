94bda36eef77c4dfc49dfc2145ac52d01777e50c
Michelle Cor <nmich.cor@gmail.com>
2024-03-21 18:10:33 +0100

    ✨ feat load more data component

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,24 @@
 import Accordian from "@/components/accordion";
 import ImageSlider from "@/components/image-slider";
+import LoadMoreData from "@/components/load-more-data";
 import RandomColor from "@/components/ramdom-color";
 import StarRating from "@/components/star-rating";
 import Image from "next/image";
 
 export default function Home() {
   return (
-<div className="App">
-  {/* Accordian component */}
-  {/* <Accordian/> */}
-  {/* Random color component */}
-  {/* <RandomColor/> */}
-  {/* Star Rating component */}
-  {/* <StarRating noOfStarts={10}/> */}
-  {/* Image Slider component */}
-  <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"} limit={"10"}/>
-</div>
-    );
+    <div className="App">
+      {/* Accordian component */}
+      {/* <Accordian/> */}
+      {/* Random color component */}
+      {/* <RandomColor/> */}
+      {/* Star Rating component */}
+      {/* <StarRating noOfStarts={10}/> */}
+      {/* Image Slider component */}
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"} limit={"10"}/> */}
+      {/* Load More data component */}
+      <LoadMoreData/>
+
+    </div>
+  );
 }
